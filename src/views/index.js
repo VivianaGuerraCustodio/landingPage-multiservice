5c3fd8653e6d0b1d0d279cc3b8120d12f0d15b67
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-11-15 22:52:33 -0500

    objeto contenedor de los templates strings

---
+++ src/views/index.js
@@ -0,0 +1,13 @@
+import Contact  from './contact.js';
+import Galery from './galery.js';
+import LandingPage from './landing.js';
+import Error from './notFound.js';
+
+const components = {
+  galery: Galery,
+  contact: Contact,
+  landing: LandingPage,
+  error : Error,
+};
+
+export { components };

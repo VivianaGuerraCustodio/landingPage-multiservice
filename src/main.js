2d84c0b623baa44c80fef146f5ed302320698891
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-11-17 22:17:49 -0500

    clase que muestra y oculta el boton del  menu hamburguesa

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import { changeView } from './view-controller/router.js';
 const init = () => {
     changeView(window.location.hash);
     window.addEventListener('hashchange', () => changeView(window.location.hash));
+    const btnMenu = document.querySelector('.btn-menu');
+    if (btnMenu) {
+        btnMenu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.nav-list-menu');
+            menu_items.classList.toggle('show');
+        })
+    }
 };
+
 
 window.addEventListener('load', init);

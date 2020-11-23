d82583881c7ace28642c6852ff82fc643fa0cbd0
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-11-22 20:20:12 -0500

    importando los datos del archivo data.js asi se pueden extraer las imagenes que estan guardadas y mostrarlas en la vista de Galeria

--- src/views/galery.js
+++ src/views/galery.js
@@ -1,9 +1,31 @@
+import data from '../data/data.js';
 export default () => {
+
     const galeryViews = `
+    <div class="galery">
+       <div  id="div_img">
+    
+       </div>
+    </div>
     
     `
     const divElement = document.createElement('div')
     divElement.innerHTML = galeryViews;
     divElement.className = ("contenedor-general")
+    const divs_img = divElement.querySelector("#div_img");
+
+    const data_json = data.data;
+    const imagenes_data = data_json["imagenes-galeria"];
+    imagenes_data.forEach(el => {
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        div.className = "container_img";
+        img.className = "galery_img";
+        img.src = el;
+        div.appendChild(img);
+        divs_img.appendChild(div);
+        //console.log(el);
+    })
+
     return divElement;
 }

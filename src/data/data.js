d5e5b55b3b7fe62728ab58dffc567886def1505f
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-11-22 20:18:02 -0500

    data  -array de imagenes-

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export default {
+    "data": {
+        "imagenes-galeria": [
+            "../assets/mesa1.jpg",
+            "../assets/mesa2.jpg",
+            "../assets/mesa3.jpg",
+            "../assets/mesa4.jpg",
+            "../assets/mesa5.jpg",
+            "../assets/mesa6.jpg",
+            "../assets/mesa7.jpg",
+            "../assets/mesa8.jpeg",
+            "../assets/mesa9.jpg",
+            "../assets/mesa10.jpg",
+            "../assets/mesa11.jpg",
+        ]
+    }
+}
